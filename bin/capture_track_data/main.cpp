49e23d1c34a3584a3d5bd5a658812227d6456f90
Leroy McLean <leroythelegend@gmail.com>
2018-04-14 18:18:33 +1000

    Updates for tute

--- bin/capture_track_data/main.cpp
+++ bin/capture_track_data/main.cpp
@@ -4,9 +4,10 @@
 
 int main() {
 
+	pcars::Process_Decision_Tree decision_tree;
 	pcars::Process_Track track;
 	pcars::Live_Feed live;
-	pcars::Capture_Telemetry telemetry(&track, &live);
+	pcars::Capture_Telemetry telemetry(&track, &live, &decision_tree);
 
 	return 0;
 
